87b3f03fec9c724b75af6dad2767e64924a79feb
Roxanne Tabuena <roxyannet@gmail.com>
2026-08-31 09:05:20 -0700

    set up new basic

--- src/components/work/Entry/Entry.js
+++ src/components/work/Entry/Entry.js
@@ -2,10 +2,13 @@ import { Basic } from './Basic/Basic'
 import { Cover } from './Cover/Cover'
 import { Cards } from './Cards/Cards'
 import style from './entry.module.css'
-export const Entry = () => {
+export const Entry = ({entry}) => {
+    const { basic, covers, cards } = entry
     return (
         <div className={style.entry}>
             <Basic basic={basic}/>
+            <Cover cover={covers}/>
+            <Cards cards={cards}/>
         </div>
     )
 }
