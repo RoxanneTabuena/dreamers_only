f3db74638518b080296e38cfa236a79915a54ffa
Roxanne Tabuena <roxyannet@gmail.com>
2026-08-31 07:35:06 -0700

    remove console log

--- src/components/work/Background/Background.js
+++ src/components/work/Background/Background.js
@@ -1,6 +1,5 @@
 import style from './background.module.css'
 export const Background = ({color}) => {
-    console.log(color)
     return (
         <div className={style.background} style={{backgroundColor : color}}>
 
